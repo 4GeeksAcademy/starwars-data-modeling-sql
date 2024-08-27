9d29103824fb30ffc58e09700a1960b30bde1154
Gustavo Alexander <89204234+gustarting@users.noreply.github.com>
2024-08-27 22:07:53 +0000

    updating star wars data modeling

--- src/models.py
+++ src/models.py
@@ -7,26 +7,49 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+
+
+
+class Usuario(Base):
+    __tablename__ = "usuario"
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(50), nullable=False)
+    email = Column(String(120), nullable=False)
+    favoritos = relationship("Favorito", backref="usuario", lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Personaje(Base):
+    __tablename__ = "personaje"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50), nullable=False)
+    especie_id = Column(Integer, ForeignKey("especie.id"), nullable=False)
+    planeta_id = Column(Integer, ForeignKey("planeta.id"), nullable=False)
+    favoritos = relationship("Favorito", backref="personaje", lazy=True)
 
-    def to_dict(self):
-        return {}
+class Planeta(Base):
+    __tablename__="planeta"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    clima = Column(String(100), nullable=False)
+    personajes = relationship("Personaje", backref="planeta", lazy=True)
+    favoritos = relationship("Favorito", backref="planeta", lazy=True)
 
+class Especie(Base):
+    __tablename__="especie"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    planeta_id = Column(Integer, ForeignKey("planeta.id"), nullable=False)
+    personajes = relationship("Personaje", backref="especie", lazy=True)
+    favoritos = relationship("Favorito", backref="especie", lazy=True)
+
+
+class Favorito(Base):
+    __tablename__= "favorito"
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey("usuario.id"), nullable = False)
+    planeta_id = Column(Integer, ForeignKey("planeta.id"), nullable = False)
+    especie_id = Column(Integer, ForeignKey("especie.id"), nullable = False)
+    personaje_id = Column(Integer, ForeignKey("personaje.id"), nullable = False)
+ 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
